cad7629400ae3cfed9fb2f99ff3dacc1c825491f
Omerkugel12 <omerkugel12@gmail.com>
2024-06-19 16:31:23 +0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PostFeedPage from "./pages/PostFeedPage";
+import PostDetailsPage from "./pages/PostDetailsPage";
+import CreatePostPage from "./pages/CreatePostPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="posts">
+          <Route index element={<PostFeedPage />} />
+          <Route path=":postId" element={<PostDetailsPage />} />
+          <Route path="create" element={<CreatePostPage />} />
+        </Route>
+        <Route path="/user" element={<UserProfilePage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/PostFeedPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PostFeedPage() {
+  return <div>PostFeedPage</div>;
+}
+
+export default PostFeedPage;
