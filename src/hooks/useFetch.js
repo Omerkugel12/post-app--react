b53843393aa2b80682dd15ade106c9186820ac3f
Omerkugel12 <omerkugel12@gmail.com>
2024-06-20 09:20:24 +0300

    post-details

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,63 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-const useFetch = (url, options = {}) => {
-  const [data, setData] = useState([]);
+// const useFetch = (url, options = {}) => {
+//   const [data, setData] = useState([]);
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   const defaultOptions = {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   };
+
+//   useEffect(() => {
+//     if (!url) return;
+
+//     const abortController = new AbortController();
+
+//     async function fetchData() {
+//       setLoading(true);
+//       setError(null);
+//       try {
+//         const response = await fetch(url, {
+//           ...defaultOptions,
+//           ...options,
+//           signal: abortController.signal,
+//         });
+//         if (!response.ok) {
+//           throw new Error("Network response was not ok");
+//         }
+//         const result = await response.json();
+//         setData(result);
+//       } catch (error) {
+//         if (error.name === "AbortError") {
+//           console.log("Fetch aborted");
+//         } else {
+//           setError(error);
+//         }
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchData();
+
+//     return () => {
+//       if (abortController) abortController.abort();
+//     };
+//   }, [url]);
+
+//   return { data, error, loading, setData };
+// };
+
+// export default useFetch;
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useAxios = (url, options = {}) => {
+  const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -14,25 +70,22 @@ const useFetch = (url, options = {}) => {
   useEffect(() => {
     if (!url) return;
 
-    const abortController = new AbortController();
+    const source = axios.CancelToken.source();
 
     async function fetchData() {
       setLoading(true);
       setError(null);
       try {
-        const response = await fetch(url, {
+        const response = await axios({
+          url,
           ...defaultOptions,
           ...options,
-          signal: abortController.signal,
+          cancelToken: source.token,
         });
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const result = await response.json();
-        setData(result);
+        setData(response.data);
       } catch (error) {
-        if (error.name === "AbortError") {
-          console.log("Fetch aborted");
+        if (axios.isCancel(error)) {
+          console.log("Request canceled", error.message);
         } else {
           setError(error);
         }
@@ -44,11 +97,11 @@ const useFetch = (url, options = {}) => {
     fetchData();
 
     return () => {
-      if (abortController) abortController.abort();
+      source.cancel("Operation canceled by the user.");
     };
   }, [url]);
 
-  return { data, error, loading, setData };
+  return { data, error, loading };
 };
 
-export default useFetch;
+export default useAxios;
--- src/pages/PostDetailsPage.jsx
+++ src/pages/PostDetailsPage.jsx
@@ -1,61 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import useFetch from "../hooks/useFetch";
+import useAxios from "../hooks/useFetch";
 import PostsList from "../components/PostsList";
 import PostItem from "../components/PostItem";
+import axios from "axios";
 
 const POST_URL = "http://localhost:8001/posts";
 
 function PostDetailsPage() {
   const { postId } = useParams();
-  // const [post,setPost] = useState({})
-
-  // useEffect(()=>{
-  //   async function getPost() {
-
-  //   }
-  // })
-
-  // return (
-  //   <>
-
-  //   </>
-  // )
-
-  const { data, error, loading, setData } = useFetch(`${POST_URL}/${postId}`);
-  console.log("data", data);
-  console.log("data.comments", data.comments);
-  console.log("data.reactions.likes", data.reactions);
-  console.log("data.title", data.title);
+  const { data, error, loading } = useAxios(`${POST_URL}/${postId}`);
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error: {error.message}</p>;
   return (
-    <div>
-      <p>title: {data.title}</p>
-      <p>data: {data.body}</p>
-      <p>comments: {data.comments}</p>
-      {/* {data.comments.length === 0 ? (
-        <p>No comments yet</p>
-      ) : (
-        <ul>
-          comments:
-          {data.comments.map((comment) => {
-            return <li key={comment.content}>{comment.content}</li>;
-          })}
-        </ul>
-      )} */}
-      {/* {data.comments.length !== 0 ? (
-              <ul>
-                comments:
-                {data.comments.map((comment) => {
-                  return <li key={comment.content}>{comment.content}</li>;
-                })}
-              </ul>
-            ) : null} */}
-
-      {/* <p>likes: {data.reactions.likes}</p> */}
-      <p>created at: {data.createdAt}</p>
-      <p>updated at: {data.updatedAt}</p>
-      <p>User id: {data.userId}</p>
-    </div>
+    data && (
+      <div>
+        <p>title: {data.title}</p>
+        <p>data: {data.body}</p>
+        <p>comments: {data.comments}</p>
+        {data.comments.length === 0 ? (
+          <p>No comments yet</p>
+        ) : (
+          <ul>
+            comments:
+            {data.comments.map((comment) => {
+              return <li key={comment.content}>{comment.content}</li>;
+            })}
+          </ul>
+        )}
+        <p>likes: {data.reactions.likes}</p>
+        <p>created at: {data.createdAt}</p>
+        <p>updated at: {data.updatedAt}</p>
+        <p>User id: {data.userId}</p>
+      </div>
+    )
   );
 }
 
